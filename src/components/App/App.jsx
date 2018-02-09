d79811eee42d28e1ba5f96ab5c7f6843232fe729
Vincent Van Dintel <vincentvandintel@gmail.com>
2018-02-08 23:11:41 -0500

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => (
+  <div className='App'>
+    {/* <img className="App-Logo" src={logo} alt="React Logo" /> */}
+    <h1 className='App-Title'>React + Parcel</h1>
+  </div>
+)
+
+export default App
